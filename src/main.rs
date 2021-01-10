dfe30a5fcb221aab66eb536f70f8ff756c430791
nutmos <nuttapong_mos@hotmail.com>
2021-01-10 21:45:02 +0700

    change to add partitions count

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::{App, Arg};
 use futures::StreamExt;
-use log::{info, debug};
+use log::{info, debug, error};
+use std::time::Duration;
 
 use rdkafka::client::ClientContext;
 use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
@@ -15,6 +16,7 @@ use rdkafka::util::get_rdkafka_version;
 //use crate::example_utils::setup_logger;
 
 use std::net::SocketAddr;
+use std::mem::drop;
 use prometheus_exporter_base::{render_prometheus, MetricType, PrometheusMetric};
 use futures::future::{ok};
 //use futures::join;
@@ -24,6 +26,8 @@ use std::sync::Mutex;
 
 lazy_static! {
     static ref KAFKA_STAT: Mutex<Option<Statistics>> = Mutex::new(None);
+    static ref KAFKA_BROKERS: Mutex<Option<String>> = Mutex::new(None);
+    static ref KAFKA_CONSUMER_GROUP_ID: Mutex<Option<String>> = Mutex::new(None);
 }
 
 //mod example_utils;
@@ -38,6 +42,7 @@ impl ClientContext for CustomContext {
         let mut guard = KAFKA_STAT.lock().unwrap();
         debug!("Get Stat Completed {:?}", statistics);
         *guard = Some(statistics);
+        drop(guard);
     }
 }
 
@@ -163,6 +168,13 @@ async fn main() {
     let brokers = matches.value_of("brokers").unwrap();
     let group_id = matches.value_of("group-id").unwrap();
 
+    let mut brokers_mutex = KAFKA_BROKERS.lock().unwrap();
+    *brokers_mutex = Some(String::from(brokers));
+    drop(brokers_mutex);
+    let mut group_id_mutex = KAFKA_CONSUMER_GROUP_ID.lock().unwrap();
+    *group_id_mutex = Some(String::from(group_id));
+    drop(group_id_mutex);
+
     let kafka_async = consume_and_print(brokers, group_id, &topics);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 32221));
@@ -179,17 +191,75 @@ async fn main() {
                 info!("Get stat for broker {} completed: {} brokers", stat.name, stat.brokers.keys().len());
                 for broker in stat.brokers.iter() {
                     let (key, val) = broker;
-                    info!("Get stat for broker {}", val.name);
-                    let mut labels = Vec::new();
-                    labels.push(("kafka_broker", &val.name[..]));
-                    let pc = PrometheusMetric::new("kafka_topics_count", MetricType::Gauge, "Kafka Topics Count");
-                    s += &pc.render_header();
-                    s.push_str(&pc.render_sample(Some(&labels[..]), stat.brokers.keys().len(), None));
+                    //info!("Get stat for broker {} {}", key, val.name);
+                    if key != "GroupCoordinator" {
+                        let mut labels = Vec::new();
+                        labels.push(("kafka_broker", &key[..]));
+                        let pc = PrometheusMetric::new("kafka_broker_partitions_count", MetricType::Gauge, "Kafka Broker Partitions Count");
+                        s += &pc.render_header();
+                        s.push_str(&pc.render_sample(Some(&labels[..]), val.toppars.keys().len(), None));
+                    } else {
+                        let mut labels = Vec::new();
+                        labels.push(("kafka_group_coordinator_broker", &val.nodename[..]));
+                        let pc = PrometheusMetric::new("kafka_group_coordinator_broker", MetricType::Gauge, "Kafka Coordinator Broker");
+                        s += &pc.render_header();
+                        s.push_str(&pc.render_sample(Some(&labels[..]), 1, None));
+                    }
                 }
                 info!("Get stat for broker {} completed: {} topics", stat.name, stat.topics.keys().len());
                 for topic in stat.topics.iter() {
                     let (key, val) = topic;
                     info!("Get stat for topic {}", val.topic);
+                }
+                info!("Get stat completed");
+                let context = CustomContext;
+                let mut group_id = "";
+                let mut group_id_guard = KAFKA_CONSUMER_GROUP_ID.lock().unwrap();
+                match &*group_id_guard {
+                    None => error!("Kafka consumer group id not set"),
+                    Some(g) => {
+                        group_id = g;
+                    }
+                }
+                let mut brokers = "";
+                let mut brokers_guard = KAFKA_BROKERS.lock().unwrap();
+                match &*brokers_guard {
+                    None => error!("Kafka brokers not set"),
+                    Some(b) => {
+                        brokers = b;
+                    }
+                }
+                
+                info!("Start consumer");
+                let consumer: LoggingConsumer = ClientConfig::new()
+                    .set("group.id", group_id)
+                    .set("bootstrap.servers", brokers)
+                    .set("enable.partition.eof", "false")
+                    .set("session.timeout.ms", "6000")
+                    .set("enable.auto.commit", "true")
+                    .set("statistics.interval.ms", "30000")
+                    //.set("auto.offset.reset", "smallest")
+                    .set_log_level(RDKafkaLogLevel::Debug)
+                    .create_with_context(context)
+                    .expect("Consumer creation failed");
+                drop(group_id_guard);
+                drop(brokers_guard);
+                let metadata = consumer.fetch_metadata(None, Duration::from_millis(60000));
+                match metadata {
+                    Err(e) => info!("Fetch Metadata Error"),
+                    Ok(m) => {
+                        let topics = m.topics();
+                        let pc = PrometheusMetric::new("kafka_topics_count", MetricType::Gauge, "Kafka Topics Count");
+                        s += &pc.render_header();
+                        s.push_str(&pc.render_sample(None, topics.len(), None));
+                        for topic in topics.iter() {
+                            let mut labels = Vec::new();
+                            labels.push(("kafka_topic_name", topic.name()));
+                            let pc = PrometheusMetric::new("kafka_topic_partitions_count", MetricType::Gauge, "Kafka Topic Partitions Count");
+                            s += &pc.render_header();
+                            s.push_str(&pc.render_sample(Some(&labels[..]), topic.partitions().len(), None));
+                        }
+                    },
                 }
             },
         }
